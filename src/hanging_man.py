f5345de64c174f65f922be52cb4ef9c94c4a395e
captainaomi <naomimowbray@live.com>
2023-04-30 17:25:44 +1000

    better useage of color package

--- src/hanging_man.py
+++ src/hanging_man.py
@@ -1,51 +1,92 @@
-man = {
-  0:  """
-  _______
-        |
-        |
-        |
-        |
-  ______|""", 
-  1: """
-  _______
-     |  |
-     O  |
-        |
-        |
-  ______|""",
-  2: """
-  _______
-     |  |
-    \O  |
-        |
-        |
-  ______|""",
-  3: """
-  _______
-     |  |
-    \O/ |
-        |
-        |
-  ______|""",
-  4: """
-  _______
-     |  |
-    \O/ |
-     |  |
-        | 
-  ______|""",
-  5: """
-  _______
-     |  |
-    \O/ |
-     |  |
-    /   | 
-  ______|""",
-  6: """
-  _______
-     |  |
-     o  |
-    /|\ |
-    / \ | 
-  ______|"""
+from colored import fg, bg, attr
+
+hanging = {  
+   8: f"""
+      {bg(15)} ________ {attr("reset")}
+      {bg(15)} |        {attr("reset")}
+      {bg(15)} |        {attr("reset")}
+      {bg(15)} |        {attr("reset")}
+      {bg(15)} |        {attr("reset")}
+      {bg(15)} |_______ {attr("reset")}
+      {bg(15)}          {attr("reset")}
+      8 turns left""",
+
+   7: f"""
+      {bg(227)} ________ {attr("reset")}
+      {bg(227)} |   |    {attr("reset")}
+      {bg(227)} |        {attr("reset")}
+      {bg(227)} |        {attr("reset")}
+      {bg(227)} |        {attr("reset")}
+      {bg(227)} |_______ {attr("reset")}
+      {bg(227)}          {attr("reset")}
+      7 turns left""",
+
+   6: f"""
+      {bg(220)} ________ {attr("reset")}
+      {bg(220)} |   |    {attr("reset")}
+      {bg(220)} |   o    {attr("reset")}
+      {bg(220)} |        {attr("reset")}
+      {bg(220)} |        {attr("reset")}
+      {bg(220)} |_______ {attr("reset")}
+      {bg(220)}          {attr("reset")}
+      6 turns left""",
+
+   5: f"""
+      {bg(214)} ________ {attr("reset")}
+      {bg(214)} |   |    {attr("reset")}
+      {bg(214)} |   o    {attr("reset")}
+      {bg(214)} |   |    {attr("reset")}
+      {bg(214)} |        {attr("reset")}
+      {bg(214)} |_______ {attr("reset")}
+      {bg(214)}          {attr("reset")}
+      5 turns left""",
+   4: f"""
+      {bg(208)} ________ {attr("reset")}
+      {bg(208)} |   |    {attr("reset")}
+      {bg(208)} |  \o    {attr("reset")}
+      {bg(208)} |   |    {attr("reset")}
+      {bg(208)} |        {attr("reset")}
+      {bg(208)} |_______ {attr("reset")}
+      {bg(208)}          {attr("reset")}
+      4 turns left""",
+
+   3: f"""
+      {bg(196)} ________ {attr("reset")}
+      {bg(196)} |   |    {attr("reset")}
+      {bg(196)} |  \o/   {attr("reset")}
+      {bg(196)} |   |    {attr("reset")}
+      {bg(196)} |        {attr("reset")}
+      {bg(196)} |_______ {attr("reset")}
+      {bg(196)}          {attr("reset")}
+      3 turns left""",
+
+   2: f"""
+      {bg(88)} ________ {attr("reset")}
+      {bg(88)} |   |    {attr("reset")}
+      {bg(88)} |  \o/   {attr("reset")}
+      {bg(88)} |   |    {attr("reset")}
+      {bg(88)} |        {attr("reset")}
+      {bg(88)} |_______ {attr("reset")}
+      {bg(88)}          {attr("reset")}
+      2 turns left""",
+
+   1: f"""
+      {bg(52)} ________ {attr("reset")}
+      {bg(52)} |   |    {attr("reset")}
+      {bg(52)} |  \o/   {attr("reset")}
+      {bg(52)} |   |    {attr("reset")}
+      {bg(52)} |        {attr("reset")}
+      {bg(52)} |_______ {attr("reset")}
+      {bg(52)}          {attr("reset")}
+      Oh gosh, you're on your last chance...""",
+
+   0:  f"""
+      {bg(0)} ________ {attr("reset")}
+      {bg(0)} |   |    {attr("reset")}
+      {bg(0)} |  \o/   {attr("reset")}
+      {bg(0)} |   |    {attr("reset")}
+      {bg(0)} |        {attr("reset")}
+      {bg(0)} |_______ {attr("reset")}
+      {bg(0)}          {attr("reset")}
+      Ba-bow, you lose (and so does our unfortunate man)."""
 }
--- src/test_hangman.py
+++ src/test_hangman.py
@@ -1,5 +1,6 @@
 import random
 import string
+from hanging_man import hanging
 from words import potential_words
 from colored import fg, bg, attr
 
@@ -12,127 +13,41 @@ def hangman_game():
     ]
     mystery_word = random.choice(potential_words).upper()
     chances = 9
-    guessed = ""
+    guessed_letters = set()
+    word_letters = set(mystery_word)
     letters = set('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
-    while len(mystery_word) > 0:
+    while len(word_letters) > 0 and chances > 0:
         player_word = ""
-
+        
         for letter in mystery_word:
-            if letter in guessed:
+            if letter in guessed_letters:
                 player_word = player_word + letter
             else:
                 player_word = player_word + "_ "
 
-        if player_word == mystery_word:
-            print(f"Nice! You figured out the word is {mystery_word}, won Python Hangman, and saved the man!")
-            break
+        print(f"\nThis is our mystery word so far: {fg(117)}{player_word}{attr(0)}")
+        print("Pssst: these are the other letters you've already guessed: ", " ".join(guessed_letters))
+        guess = input("\nWhat letter do you want to try? \n").upper()
 
-        print(f"\nOkay, let's guess a letter in this mystery word: {player_word}")
-        print("Pssst: these are the ones you've already guessed: ", " ".join(guessed))
-        guess = input().upper()
 
-        if guess in letters:
-            if guess in guessed:
-                print(f"You already tried {guess}, so let's go with a different letter.")
-            guessed = guessed + guess
+        if guess in letters - guessed_letters: #ie. still in alphabet, minus the ones we've tried
+            guessed_letters.add(guess)
+            if guess in word_letters:
+                word_letters.remove(guess)
+            else:
+                chances = chances - 1
+                print(hanging[chances])
+
+        elif guess in guessed_letters:
+            print(f"You already tried {guess}, so let's go with a different letter.")
         else:
             print(f"Come on, {guess} isn't a letter, you're gunna have to give it another go.")
-
-        if guess not in mystery_word:
-            chances = chances - 1
-            if chances == 8:
-                color = bg(15) + fg(0)
-                reset = attr('reset')
-                print(color + "________" + reset)
-                print(color + "|       " + reset)
-                print(color + "|       " + reset)
-                print(color + "|       " + reset)
-                print(color + "|       " + reset)
-                print(color + "|_______" + reset)
-                print("8 turns left")
-            if chances == 7:
-                color = bg(227) + fg(0)
-                reset = attr('reset')
-                print(color + "________" + reset)
-                print(color + "|   |   " + reset)
-                print(color + "|       " + reset)
-                print(color + "|       " + reset)
-                print(color + "|       " + reset)
-                print(color + "|_______" + reset)
-                print("7 turns left")
-            if chances == 6:
-                color = bg(220) + fg(0)
-                reset = attr('reset')
-                print(color + "________" + reset)
-                print(color + "|   |   " + reset)
-                print(color + "|   o   " + reset)
-                print(color + "|       " + reset)
-                print(color + "|       " + reset)
-                print(color + "|_______" + reset)
-                print("6 turns left")
-            if chances == 5:
-                color = bg(214) + fg(0)
-                reset = attr('reset')
-                print(color + "________" + reset)
-                print(color + "|   |   " + reset)
-                print(color + "|   o   " + reset)
-                print(color + "|   |   " + reset)
-                print(color + "|       " + reset)
-                print(color + "|_______" + reset)
-                print("5 turns left")
-            if chances == 4:
-                color = bg(208) + fg(0)
-                reset = attr('reset')
-                print(color + "________" + reset)
-                print(color + "|   |   " + reset)
-                print(color + "|  \o   " + reset)
-                print(color + "|   |   " + reset)
-                print(color + "|       " + reset)
-                print(color + "|_______" + reset)
-                print("4 turns left")
-            if chances == 3:
-                color = bg(196) + fg(0)
-                reset = attr('reset')
-                print(color + "________" + reset)
-                print(color + "|   |   " + reset)
-                print(color + "|  \o/  " + reset)
-                print(color + "|   |   " + reset)
-                print(color + "|       " + reset)
-                print(color + "|_______" + reset)
-                print("3 turns left")
-            if chances == 2:
-                color = bg(88) + fg(0)
-                reset = attr('reset')
-                print(color + "________" + reset)
-                print(color + "|   |   " + reset)
-                print(color + "|  \o/  " + reset)
-                print(color + "|   |   " + reset)
-                print(color + "|  /    " + reset)
-                print(color + "|_______" + reset)
-                print("2 turns left")
-            if chances == 1:
-                color = fg(0) + bg(52)
-                reset = attr('reset')
-                print(color + "________" + reset)
-                print(color + "|   |   " + reset)
-                print(color + "|  \o/  " + reset)
-                print(color + "|   |   " + reset)
-                print(color + "|  / \  " + reset)
-                print(color + "|_______" + reset)
-                print("Oh gosh, you're on your last chance...")
-            if chances == 0:
-                color = fg(0) + bg(0)
-                reset = attr('reset')
-                print(color + "________" + reset)
-                print(color + "|   |   " + reset)
-                print(color + "|   o   " + reset)
-                print(color + "|  /|\  " + reset)
-                print(color + "|  / \  " + reset)
-                print(color + "|_______" + reset)
-                print(f'''Ba-bow, you lose (and so does our unfortunate man).
-The word was {mystery_word}. Better luck next time!''')
-                break
+            
+    if chances == 0:
+        print(f"\nThe word was {fg(196)}{mystery_word}{attr(0)}. Better luck next time!")
+    else:
+        print(f"Nice! You figured out the word is {fg(117)}{mystery_word}{attr(0)}, won Python Hangman, and saved the man!")
 
 
 # name = input("Hi, what's your name? ")
