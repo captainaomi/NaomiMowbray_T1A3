7575d4bdf611f62bc701fa2041bb0147b4974c5b
captainaomi <naomimowbray@live.com>
2023-05-04 11:55:11 +0800

    ensured all lines of code are less than 79 characters, which took aaaaages but adheres correctly to pep8 now!

--- src/draft_hangman.py
+++ src/draft_hangman.py
@@ -34,7 +34,8 @@ retrieved = ""
 
 def intro(file, retrieved):
     global name
-    name = input(emoji.emojize("Helllllo there :clown_face: What's your name? "))
+    name = input(emoji.emojize(
+        "Helllllo there :clown_face: What's your name? "))
     global wins 
     wins = 0
     global losses
@@ -69,7 +70,7 @@ def hangman_game(mystery_word, word_letters):
     global chances 
     chances = 9
     guessed = set()
-    letters = set('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    letters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
     while len(word_letters) > 0 and chances > 0:
         player_word = ""
@@ -80,12 +81,13 @@ def hangman_game(mystery_word, word_letters):
             else:
                 player_word = player_word + "_ "
 
-        print(f"\nThis is our mystery word so far: {fg(117)}{player_word}{attr(0)}")
-        print("When you make a guess, the letters will show here: ", " ".join(guessed))
+        print(f"\nThis is our word so far: {fg(117)}{player_word}{attr(0)}")
+        print("When you make a guess, it'll show here: ", " ".join(guessed))
         guess = input("What letter do you want to try? ").upper()
 
 
-        if guess in letters - guessed: #ie. still in alphabet, minus the ones we've tried
+        if guess in letters - guessed: 
+        #ie. still in alphabet, minus the ones we've tried
             guessed.add(guess)
             if guess in word_letters:
                 word_letters.remove(guess)
@@ -94,18 +96,23 @@ def hangman_game(mystery_word, word_letters):
                 print(hanging[chances])
 
         elif guess in guessed:
-            print(f"\nYou already tried {guess}, so can we try a different letter?")
+            print(
+                f"\nYou already tried {guess}. Maybe try a different letter?")
         else:
-            print(emoji.emojize(f"\nCome on {name}, {guess} isn't a letter :woozy_face: Let's give that another go."))
+            print(emoji.emojize(
+                f"""\nCome on {name}, {guess} isn't a letter :woozy_face:
+Let's give that another go..."""))
             
 
 
 def did_he_die(chances):
     if chances == 0:
-        print(emoji.emojize(f"""\nDaaaang {name}, the word was {fg(196)}{mystery_word}{attr(0)}. 
+        print(emoji.emojize(
+            f"""\nDang {name}, the word was {fg(196)}{mystery_word}{attr(0)}. 
 Better luck next time! :crossed_fingers:"""))
     else:
-        print(emoji.emojize(f"""\nNice job {name}, you legend! :partying_face:
+        print(emoji.emojize(
+            f"""\nNice job {name}, you legend! :partying_face:
 The word was {fg(117)}{mystery_word}{attr(0)} - you saved the man!"""))
 
 
@@ -135,9 +142,12 @@ def outcome(chances, losses, wins):
 
 def main():
     while True:
-        play = input(f"Do you want to play Python Hangman and save this fella? (yes/no) ").upper()[0]
+        play = input(
+            f"Do you want to play Python Hangman and save this fella? (y/n) "
+            ).upper()[0]
         if play == "N":
-            print(emoji.emojize(f"No worries {name}, let's play later instead :waving_hand:"))
+            print(emoji.emojize(
+                f"No worries {name}, let's play later instead :waving_hand:"))
             break
         elif play == "Y":
             print(f"Righto {name}, let's do this!")
@@ -146,7 +156,8 @@ def main():
             did_he_die(chances)
             outcome(chances, losses, wins)
         else:
-            print(emoji.emojize("Innnnteresting, that's not a yes OR a no... :thinking_face:"))
+            print(emoji.emojize(
+                "Innnnteresting, that's not a yes OR a no... :thinking_face:"))
             
 
 intro(file, retrieved)
